8fcb17ef3cffd00d46aec38da7248fe2e782a256
Fabri <chubyxd1627@gmail.com>
2025-08-23 20:17:04 -0400

    codigo de matrices corregido

--- Math/matrix2.cpp
+++ Math/matrix2.cpp
@@ -1,28 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int mod = 1e9+7;
+typedef long long ll;
 typedef vector<int> row;
 typedef vector<row> Mat;
 
 Mat matMul(Mat &a,Mat &b){
-  Mat c(a.size(),row(b.size(),0));
-  for(int i=0;i<c.size();i++){
-    for(int j=0;j<c[i].size();j++){
-      for(int k=0;k<b.size();k++){
-	c[i][j]+=a[i][k]*b[k][j];
+  int n = a.size();
+  int m = a[0].size();
+  int p = b[0].size();
+  Mat c(n,row(p,0));
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      for(int k=0;k<p;k++){
+	c[i][k]=(c[i][k]%mod+(a[i][j]%mod*b[j][k]%mod)%mod)%mod;
       }
     }
   }
   return c;
 }
-
-Mat MatPow(Mat A, int p){
+Mat MatPow(Mat A, ll p){
   Mat ans(A.size(),row(A.size(),0));
   for(int i=0;i<A.size();i++)ans[i][i]=1;
   while(p){
     if(p&1)ans=matMul(ans,A);
     A = matMul(A,A);
-    p>>1;
+    p>>=1;
   }
   return ans;
 }
